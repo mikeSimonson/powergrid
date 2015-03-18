55608a7d539dccc9d70c8a74401e82a1396eb22a
Eddie Peters <edward.paul.peters@gmail.com>
2015-03-17 23:09:15 -0400

    Set up part of the register user route.

---
+++ apilib/JSONResult.php
@@ -0,0 +1,90 @@
+<?php
+namespace Api;
+
+class JSONResult {
+  const RESULT_ERROR = 'ERROR';
+  const RESULT_SUCCESS = 'SUCCESS';
+
+  const RESULT_STATUS_KEY = 'status';
+  const RESULT_MESSAGE_KEY = 'message';
+  const RESULT_ERRORS_KEY = 'errors';
+  
+  protected $reservedDataKeys = array();
+
+  protected $status = null;
+  protected $errors = array();
+  protected $successMessage = array();
+  protected $data = array();
+
+  public function __construct() {
+    $this->reservedDataKeys = array(
+      static::RESULT_STATUS_KEY,
+      static::RESULT_MESSAGE_KEY,
+      static::RESULT_ERRORS_KEY
+    );
+  }
+
+  public function addError($errorMsg) {
+    if (!is_string($errorMsg)) {
+      throw new Exception("JSON result messages must be strings.");
+    }
+
+    $this->status = static::RESULT_ERROR;
+
+    $this->errors[] = $errorMsg;
+  }
+
+  public function setSuccess($successMsg) {
+    if (!$this->noErrors()) {
+      throw new Exception("Trying to set success on a JSON result when there are errors.");
+    }
+
+    if (!is_string($successMsg)) {
+      throw new Exception("JSON result messages must be strings.");
+    }
+
+    $this->status = static::RESULT_SUCCESS;
+    $this->successMsg = $successMsg;
+  }
+
+  public function noErrors() {
+    return ($this->status !== static::RESULT_ERROR) && (count($this->errors) == 0);
+  }
+
+  public function send() {
+    $result = new \stdClass();
+
+    $result->status = $this->status;
+
+    if ($this->noErrors()) {
+      $result->message = $this->successMsg;
+    }
+    else {
+      $result->errors = $this->errors;
+    }
+
+    $result->data = $this->data;
+
+    echo $this->JSONify($result);
+  }
+
+  public function addData($key, $value) {
+    if (in_array($key, $this->reservedDataKeys)) {
+      throw new Exception('The following keys are reserved and may not be used in JSON result data: ' . implode(' ', $this->reserved_data_keys));
+    }
+
+    $this->data[$key] = $value;
+  }
+
+  public function JSONify($thing) {
+    $json = json_encode($thing);
+
+    if ($json === false) {
+      throw new Exception("JSON result only accepts jsonifiable things.");
+    }
+
+    return $json;
+  }
+}
+
+?>
---
+++ public_html/index.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once('../bootstrap.php');
+
+$app = new \Slim\Slim();
+
+$app->post('/register', function() {
+  $json_result = new \Api\JSONResult();
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+
+  $username_min_len = 3;
+  $username_max_len = 40;
+  $username_pcre_range = '{' . $username_min_len . ',' . $username_max_len . '}';
+  $username_match_pattern = '/^\w' . $username_pcre_range . '$/';
+
+  $pwd_normal_chars = '[:alnum:]';
+  $pwd_special_chars = '!@#$%^&*()';
+  $pwd_min_len = 8;
+  $pwd_max_len = 40;
+  $pwd_pcre_range = '{' . $pwd_min_len . ',' . $pwd_max_len . '}';
+  $pwd_match_pattern = '/^[\w' . $pwd_special_chars . ']' . $pwd_pcre_range . '$/i';
+
+  //@todo: make sure username is not already taken
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $json_result->addError('Invalid email address.');
+  }
+  
+  if (!preg_match($username_match_pattern, $username)) {
+    $json_result->addError("Invalid username. Must contain only alphanumerics and '_' and be between $username_min_len and $username_max_len chars.");
+  }
+
+  if (!preg_match($pwd_match_pattern, $password)) {
+    $json_result->addError("Invalid password. Can only contain chars A-Z, a-z, 0-9, and the special chars $pwd_special_chars. Must be between $pwd_min_len and $pwd_max_len chars.");
+  }
+
+  if ($json_result->noErrors()) {
+    $json_result->setSuccess('Username ' . $username . ' has been created.');
+    $json_result->addData('token', 'someunguessablenumber');
+    // Do username operation
+  }
+
+  $json_result->send();
+});
+
+$app->run();
+
+?>
