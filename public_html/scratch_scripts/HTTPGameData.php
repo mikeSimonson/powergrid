071b085d3b87fa1218706bfce2ca8093d9b43ce3
Eddie Peters <edward.paul.peters@gmail.com>
2015-04-23 12:49:04 -0400

    Verify that players can easily be gotten from the gameData class.

--- public_html/scratch_scripts/HTTPGameData.php
+++ public_html/scratch_scripts/HTTPGameData.php
@@ -4,12 +4,12 @@ require('../../bootstrap.php');
 
 $gameData = new \HTTPPowerGrid\GameData();
 
+for ($i = 0; $i < 6; $i++) {
+  $player = new Player();
+  $player->setCurrentGame($gameData);
+  $player->save();
+}
+
 $gameData->save();
 
-var_dump($gameData->getId());
-
-$existingGameData = new GameQuery();
-
-$game2 = $existingGameData->findPK(2);
-
-var_dump($game2->getTurnNumber());
+var_dump($gameData->getPlayers());
