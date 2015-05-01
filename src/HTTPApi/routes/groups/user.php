734fe96e7caf6c5fa37b333326740dd33ca469e2
Eddie Peters <edward.paul.peters@gmail.com>
2015-05-01 10:07:20 -0400

    Started setting up login endpoint. Need to add some methods to the propel generated classes for the token table.

--- src/HTTPApi/routes/groups/user.php
+++ src/HTTPApi/routes/groups/user.php
@@ -88,4 +88,66 @@ $app->group('/user', function () use ($app, $json_result) {
     $app->response->setStatus(HTTPResponse::HTTP_OK);
     $app->response->setBody($json_result->getJSON());
   });
+
+  $app->post('/login', function() use($app, $json_result) {
+    $username = $app->request->post('username');
+    $password = $app->request->post('password');
+
+    // Ensure that we got a username and password.
+    $expected_indices = array('username', 'password');
+    $input_map = array(
+      'username' => $username,
+      'password' => $password
+    );
+    foreach ($input_map AS $input) {
+      if (empty($input)) {
+        $app->response->setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+        $json_result->addError('Expected username and password.');
+        $app->response->setBody($json_result->getJSON());
+        return;
+      }
+    }
+
+    // Validate username and password
+    $q = UserQuery::create();
+    $user = $q->findOneByUsername($username);
+    if (is_null($user) || !password_verify($password, $user->getPassword())) {
+      $app->response->setStatus(HTTPResponse::BAD_REQUEST);
+      $json_result->addError('Username or password not found/incorrect.');
+      $app->response->setBody($json_result->getJSON());
+      return;
+    }
+
+    // Get the existing token or generate a new one based on timestamp.
+    $q = UserTokenQuery::create();
+    $userToken = $q->findOneByUser($user);
+    if (is_null($userToken)) {
+      $userToken = new \UserToken();
+      $userToken->generateNewToken();
+    }
+    else {
+      if ($userToken->isExpired()) {
+        $userToken->generateNewToken();
+      }
+    }
+
+    $userToken->save();
+    
+    $returnToken = $userToken->getToken();
+    $expirationDate = $userToken->getExpirationDate();
+
+    // All good. Send the token information along.
+    $json_result->setSuccess('Valid credentials. Token information in data portion of response.');
+    $json_result->addData('token', $returnToken);
+    $json_result->addData('expires', $expirationDate);
+    $app->response->setStatus(HTTPResponse::HTTP_OK);
+    $app->response->setBody($json_result->getJSON());
+    return;
+  });
+
+  // Used if the user already has an account, and wants to manage it.
+  $app->group('/account', function() use ($app, $json_result) {
+
+  }); // END /user/account endpoint
+
 });
