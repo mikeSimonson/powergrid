f874b866dc60b987cf53c8507f1c625f9e281cc8
Eddie Peters <edward.paul.peters@gmail.com>
2015-07-20 17:32:44 -0400

    EOD commit.

--- src/PowerGrid/Services/GamePlayerManager.php
+++ src/PowerGrid/Services/GamePlayerManager.php
@@ -9,11 +9,15 @@ class GamePlayerManager {
     $this->gameData = $game;
   }
 
-  public function isPlayerInGame($player) {
+  public function isPlayerInGame(\Player $player) {
     $playerInGame = FALSE;
     if ($this->gameData->getId() === $player->getGameId()) {
       $playerInGame = TRUE;
     }
     return $playerInGame;
+  }
+
+  public function joinPlayerToGame(\Player $game) {
+    $this->addPlayerToGame($player);
   }
 }
---
+++ src/PowerGrid/Services/PlayerServices.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PowerGrid\Services;
+
+class PlayerServices {
+  protected $player;
+
+  public __construct(\Player $player) {
+    $this->player = $player;
+  }
+
+  public setPlayerName($name) {
+
+  }
+
+}
