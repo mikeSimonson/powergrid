d99dc6be78e00033e89bfce9b2fa89d0740040a0
Eddie Peters <edward.paul.peters@gmail.com>
2015-07-24 13:39:12 -0400

    Add some needed HTTP PowerGrid exceptions.

---
+++ src/HTTPPowerGrid/Exceptions/Administrative/PlayerDoesNotExist.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace HTTPPowerGrid\Exceptions\Administrative;
+
+class PlayerDoesNotExist extends \HTTPPowerGrid\Exceptions\Administrative {
+  protected $defaultMessage = 'Player does not exist';
+}
---
+++ src/HTTPPowerGrid/Exceptions/Administrative/UserCannotControlPlayer.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace \HTTPPowerGrid\Exceptions\Administrative;
+
+class UserCannotControlPlayer extends \HTTPPowerGrid\Exceptions\Administrative {
+  protected $defaultMessage = 'User cannot control player';
+}
