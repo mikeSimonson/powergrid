194014fe540139b2f60243ca44b6b9b7359f8893
Eddie Peters <edward.paul.peters@gmail.com>
2015-09-29 13:27:30 -0400

    Add CityConnectionsGraph class.

---
+++ src/PowerGrid/Structures/CityConnectionsGraph.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PowerGrid\Structures;
+
+class CityConnectionsGraph {
+
+  protected $cityNodeRefHash;
+  protected $shortestPathFinder;
+
+  public function __construct(\PowerGrid\Services\ShortestPathFinder $shortestPathFinder) {
+    $this->shortestPathFinder = $shortestPathFinder;
+  }
+
+  public function populateFromIncompleteGraphNodes(Array $incompleteGraph) {
+    $this->saveNodeRefs($incompleteGraph);
+    $this->buildFullGraph();
+  }
+
+  public function findShortestPathBetweenCities($fromCity, $toCity) {
+    $this->shortestPathFinder->setStartNode($this->getStartNode($fromCity));
+    $this->shortestPathFinder->setEndNode($toCity);
+    return $this->shortestPathFinder->getShortestPath();
+  }
+
+  public function getStartNode($requestedNode) {
+    $startNode = NULL;
+
+    if ($requestedNode instanceof \PowerGrid\Structures\CityNode && isset($this->cityNodeRefHash[$requestedNode->getId()])) {
+      $startNode = $this->cityNodeRefHash[$requestedNode->getId()];
+    }
+    else if (is_numeric($requestedNode)) {
+      $startNode = $this->cityNodeRefHash[$requestedNode];
+    }
+    else if ($requestedNode === NULL) {
+      return array_rand($this->cityNodeRefHash);
+    }
+    else {
+      throw new \PowerGrid\Exceptions\Application\RequestedStartNodeNotInGraph();
+    }
+
+    return $startNode;
+  }
+
+  protected function saveNodeRefs(Array $nodes) {
+    foreach ($nodes AS $node) {
+      if (isset($this->cityNodeRefHash[$node->getId()])) {
+        $this->cityNodeRefHash[$node->getId()] &= $node;
+      }
+    }
+  }
+
+  protected function buildFullGraph() {
+    foreach ($this->cityNodeRefHash AS $cityNode) {
+      $this->connectNeighborsBackToTarget($cityNode);
+    }
+  }
+
+  /**
+   * Make the connections between a node and its neighbors undirected (bidirectional).
+   */
+  private function connectNeighborsBackToTarget($target) {
+    $this->saveNodeRefs($target->getNeighbors());
+    foreach ($target->getNeighbors() AS $neighbor) {
+      $neighbor->addNeighbor($target, $target->getConnectionWeightForNeighbor($neighbor));
+    }
+  }
+}
