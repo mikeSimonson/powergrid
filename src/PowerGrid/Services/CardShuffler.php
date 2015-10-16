be7f55e63ed6d4c2ed235b005b1e84258a37129d
Eddie Peters <edward.paul.peters@gmail.com>
2015-10-15 22:31:01 -0400

    Missing some stuff. Incomplete file.

--- src/PowerGrid/Services/CardShuffler.php
+++ src/PowerGrid/Services/CardShuffler.php
@@ -5,7 +5,7 @@ namespace PowerGrid\Services;
 class CardShuffler implements \PowerGrid\Interfaces\Service {
 
   public function __construct(\PowerGrid\Interfaces\CardsData $cards) {
-  
+    $this->
   }
 
 }
