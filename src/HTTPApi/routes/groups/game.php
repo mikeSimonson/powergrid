4f7f073492f56dd45aeee8ff4b8df72099e988e0
Eddie Peters <edward.paul.peters@gmail.com>
2015-10-26 15:09:40 -0400

    Get start route working on HTTPApi level.

--- src/HTTPApi/routes/groups/game.php
+++ src/HTTPApi/routes/groups/game.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * All this route code is pretty shitty because each route could be widdled 
+ * down to just a couple of lines if I ahd proper services for the HTTP layer. 
+ * One day I will.
+ */
+
 require_once('../bootstrap.php');
 
 use Symfony\Component\HttpFoundation\Response as HTTPResponse;
@@ -19,6 +25,18 @@ $app->group('/game', function() use ($app, $json_result) {
   
   $app->post('/create', function() use ($app, $json_result) {
     $token = $app->request->params('token');
+
+    if (is_numeric($app->request->params('cardSet'))) {
+      $cardSet = \CardSetQuery::create()
+        ->findPK($app->request->params('cardSet'));
+    }
+    else {
+      $cardSet = \CardSetQuery::create()
+        ->filterByName('Originals')
+        ->limit(1)
+        ->findOne();
+    }
+
     $user = \HTTPPowerGrid\Services\UserServices::getUserByToken($token);
 
     $name = $app->request->params('name');
@@ -26,6 +44,7 @@ $app->group('/game', function() use ($app, $json_result) {
     $userGameCreator = new \HTTPPowerGrid\Services\UserGameCreator($user);
     $userGameCreator->setGameName($name);
     $newGame = $userGameCreator->createGame();
+    $newGame->setCardSet($cardSet);
     $newGame->save();
     $newGameId = $newGame->getId();
 
@@ -43,11 +62,19 @@ $app->group('/game', function() use ($app, $json_result) {
 
     if (is_null($game)) {
       $json_result->addError('Game with id ' . intval($gameId) . ' does not exist.');
+      $app->response->setStatus(HTTPResponse::HTTP_BAD_REQUEST);
+      $app->response->setBody($json_result->getJSON());
       return;
     }
 
+    $gameDeckSearcher = new \HTTPPowerGrid\Services\GameDeckSearcher($game);    
+    $auctionStarter = new \HTTPPowerGrid\Services\AuctionStarter($game, $gameDeckSearcher);
+
+    $cardShuffler = new \HTTPPowerGrid\Services\GameCardsShuffler($game);
+    $deckStarter = new \HTTPPowerGrid\Services\GameDeckStarter($game, $game->getCardSet(), $cardShuffler);
+
     try {
-      $gameStarter = new \HTTPPowerGrid\Services\GameStarter($game);
+      $gameStarter = new \HTTPPowerGrid\Services\GameStarter($game, $deckStarter, $auctionStarter);
       $gameStarter->setStartingUser($user);
       $gameStarter->startGame();
       $json_result->setSuccess('Game started');
