c1e3caa17a6f02fbc583a19e76c419999e50770e
Eddie Peters <edward.paul.peters@gmail.com>
2015-12-04 14:34:42 -0500

    Correct some docblocks.

--- src/PowerGrid/Interfaces/GameControls.php
+++ src/PowerGrid/Interfaces/GameControls.php
@@ -6,27 +6,32 @@ interface GameControls {
 
   /**
    * @param   int
+   * @param   int
    */
   public function startBid($playerId, $powerPlantId);
 
   /**
    * @param   int
    * @param   int
+   * @param   int
    */ 
   public function placeBid($playerId, $powerPlantId, $bidAmount);
 
   /**
+   * @param   int
    * @param array   $resource_name => $resource_quantity
    */
   public function buyResources($playerId, $resourceOrder);
 
   /**
+   * @param   int
    * @param array
    */
   public function buildCities($playerId, $cityNames);
 
 
   /**
+   * @param   int
    * @param   int
    * @param   array   $resource_name => $resource_quantity
    */
