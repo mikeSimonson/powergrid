dc02c4efb76af81e301c32ef7fd490a1c8a24e75
Eddie Peters <edward.paul.peters@gmail.com>
2015-12-31 11:32:27 -0500

    Detect test DB full path from run location of runtest.sh

--- runtests.sh
+++ runtests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-TEST_SQLITE3_DB_ABSOLUTE_PATH="/var/www/db/integration_tests_sqlite3.db"
+SCRIPT_RELATIVE_PATH="`dirname \"$0\"`"
+SCRIPT_ABSOLUTE_PATH="`( cd \"$SCRIPT_RELATIVE_PATH\" && pwd )`"
+DB_RELATIVE_PATH="/db/integration_tests_sqlite3.db"
+TEST_SQLITE3_DB_ABSOLUTE_PATH=$SCRIPT_ABSOLUTE_PATH$DB_RELATIVE_PATH
 echo $TEST_SQLITE3_DB_ABSOLUTE_PATH
 export TEST_SQLITE3_DB_ABSOLUTE_PATH
 ./tests/rununittests.sh
